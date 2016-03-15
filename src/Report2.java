4c1a180cfc5ac029ae5371545abc0af7b3586e5c
empoluboyarov <poluboyarovem@gmail.com>
2016-03-15 15:55:59 +0300

    Version 1.55

--- src/Report2.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Evgeniy on 10.03.2016.
- */
-public class Report2 extends Report{
-
-    public Report2(Client client, Dish[] dishs){
-        printReport2(client,dishs);
-    }
-}
